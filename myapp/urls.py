79128ab6a30b588cdd66f5bd4854cdd17fc2485a
LostParadise07 <shahaasrar1@gmail.com>
2024-10-11 16:19:06 +0530

    Refactor CSS and template files
    - Update image URLs in lightbox.css and templatemo-hexashop.css to use static file paths
    - Add new URL patterns for about, contact, products, and single-product pages in urls.py
    - Create new view functions for about, contact, products, and single-product pages in views.py
    - Update static file paths in contact.html to use static template tags

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("index", views.index, name="index"),
+    path("about", views.about, name="about"),
+    path("contact", views.contact, name="contact"),
+    path("products", views.products, name="products"),
+    path("single-product", views.single_product, name="single-product"),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -5,3 +5,17 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "shopify/index.html")
+
+def about(request):
+    return render(request, "shopify/about.html")
+
+def contact(request):
+    return render(request, "shopify/contact.html")
+
+def products(request):
+    return render(request, "shopify/products.html")
+
+def single_product(request):
+    return render(request, "shopify/single-product.html")
+
+
