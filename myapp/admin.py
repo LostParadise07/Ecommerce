1171135d064d93804c5d473f5d2bb4d6389314ce
LostParadise07 <shahaasrar1@gmail.com>
2024-10-15 14:08:39 +0530

    Refactor admin.py to remove Brand model registration

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Category, Brand, Product, Subcategory
+from .models import Category, Product, Subcategory
 
 admin.site.register(Category)
 admin.site.register(Subcategory)
-admin.site.register(Brand)
 admin.site.register(Product)
--- myapp/models.py
+++ myapp/models.py
@@ -20,24 +20,12 @@ class Subcategory(models.Model):
     def __str__(self):
         return self.name
 
-class Brand(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    logo = models.ImageField(upload_to='brands/', default='brands/default-logo.png')
-    description = models.TextField(blank=True, null=True)
-    website = models.URLField(blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name
-
 class Product(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)  
     subcategory = models.ForeignKey(Subcategory, on_delete=models.SET_NULL, null=True, related_name='products')  
-    brand = models.ForeignKey(Brand, on_delete=models.SET_NULL, null=True, related_name='products')
     image = models.ImageField(upload_to='products/',default='brands/default-logo.png')  
     available = models.BooleanField(default=True)  
     url_to_brand_page = models.URLField(blank=True, null=True)  
